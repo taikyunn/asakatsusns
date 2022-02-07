3f91888466007f268a74857ec72d6633d16d9090
taikyunn <taichi_141@me.com>
2022-02-07 12:59:06 +0900

    環境変数を元に戻す。

--- backend/models/db/userDb.go
+++ backend/models/db/userDb.go
@@ -3,6 +3,8 @@ package db
 import (
 	entity "app/models/entity"
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/jinzhu/gorm"
 )
@@ -17,24 +19,24 @@ func InsertUser(signUp *entity.User) {
 
 func gormConnect() *gorm.DB {
 	// err := godotenv.Load()
-	// if err != nil {
-	// 	log.Fatal("ここでエラーです。")
-	// }
+	if err != nil {
+		log.Fatal("ここでエラーです。")
+	}
 
-	// USER := os.Getenv("API_USER")
-	// PASS := os.Getenv("API_PASS")
-	// ADDRESS := os.Getenv("API_ADDRESS")
+	USER := os.Getenv("API_USER")
+	PASS := os.Getenv("API_PASS")
+	ADDRESS := os.Getenv("API_ADDRESS")
 	DBMS := "mysql"
 	DBNAME := "asakatsusns"
 
-	// if os.Getenv("DB_ENV") == "production" {
-	// 	USER = os.Getenv("DB_USER")
-	// 	PASS = os.Getenv("DB_PASS")
-	// 	ADDRESS = os.Getenv("DB_ADDRESS")
-	// }
-	USER := "admin"
-	PASS := "password"
-	ADDRESS := "asakatsusns.cvfat2usql6c.ap-northeast-1.rds.amazonaws.com"
+	if os.Getenv("DB_ENV") == "production" {
+		USER = os.Getenv("DB_USER")
+		PASS = os.Getenv("DB_PASS")
+		ADDRESS = os.Getenv("DB_ADDRESS")
+	}
+	// USER := "admin"
+	// PASS := "password"
+	// ADDRESS := "asakatsusns.cvfat2usql6c.ap-northeast-1.rds.amazonaws.com"
 
 	// コンテナ名:ポート番号を指定する
 	CONNECT := USER + ":" + PASS + "@tcp(" + ADDRESS + ":3306)" + "/" + DBNAME + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
